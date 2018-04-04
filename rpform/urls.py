61adc79deed8c340f1f209029b11b9102d98e675
Sergio Castillo <s.cast.lara@gmail.com>
2018-04-04 15:36:19 +0200

    explorer -> gexplorer

--- rpform/urls.py
+++ rpform/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 admin.autodiscover()
 urlpatterns = [
     url(r'^$', views.index_view, name='index_view'),
-    url(r'^explorer$', views.gene_explorer, name='gene_explorer'), 
+    url(r'^gexplorer$', views.gene_explorer, name='gene_explorer'), 
     url(r'^pathways$', views.pathway_explorer, name='pathway_explorer'),
     url(r'^tutorial$', views.tutorial, name='tutorial')
 ]
